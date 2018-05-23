d2803ffd01edcf2c2399f064305a98766ae37ba7
Sean Lingren <srlingren@gmail.com>
2018-05-23 12:39:14 -0500

    github token export, function to add/commit/push

--- git/functions.zsh
+++ git/functions.zsh
@@ -5,3 +5,12 @@ ga() {
     git commit -m "$*"
     git push
 }
+
+# Exports admin github personal access token
+# egt -> export github token
+egt () {
+    TOKEN=$(lpass show -p 5317731538991673017)
+    export GITHUB_PAT="$TOKEN"
+    export GITHUB_TOKEN="$TOKEN"
+    export GITHUB_ORGANIZATION="lingrino"
+}
