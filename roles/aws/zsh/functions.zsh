e2774734b0736d9cba6b66c8d1929275e3bbae1d
Sean Lingren <srlingren@gmail.com>
2018-07-19 19:51:02 -0500

    rm aws acct

--- roles/aws/zsh/functions.zsh
+++ roles/aws/zsh/functions.zsh
@@ -12,18 +12,6 @@ epc () {
     export AWS_SESSION_TOKEN=$(echo "$CREDS" | jq -r .Credentials.SessionToken)
 }
 
-epcs () {
-    unset AWS_ACCESS_KEY_ID
-    unset AWS_SECRET_ACCESS_KEY
-    unset AWS_SESSION_TOKEN
-    export AWS_ACCESS_KEY_ID=$(op get item 57bsrrdkcrgwjo2mlaqcqlrhce | jq -r '.details.sections[] | select(.fields) | select(.fields[].t == "AWS_ACCESS_KEY_ID") | .fields[] | select(.t == "AWS_ACCESS_KEY_ID") | .v')
-    export AWS_SECRET_ACCESS_KEY=$(op get item 57bsrrdkcrgwjo2mlaqcqlrhce | jq -r '.details.sections[] | select(.fields) | select(.fields[].t == "AWS_SECRET_ACCESS_KEY") | .fields[] | select(.t == "AWS_SECRET_ACCESS_KEY") | .v')
-    CREDS=$(aws sts get-session-token --serial-number arn:aws:iam::766970670128:mfa/sean --token-code $1)
-    export AWS_ACCESS_KEY_ID=$(echo "$CREDS" | jq -r .Credentials.AccessKeyId)
-    export AWS_SECRET_ACCESS_KEY=$(echo "$CREDS" | jq -r .Credentials.SecretAccessKey)
-    export AWS_SESSION_TOKEN=$(echo "$CREDS" | jq -r .Credentials.SessionToken)
-}
-
 # Assumes the admin role using my creds
 # ara -> assume role admin
 ara () {
@@ -32,18 +20,6 @@ ara () {
         --role-session-name AdminSean \
         --duration-seconds 3600 \
         --serial-number arn:aws:iam::281050604782:mfa/sean \
-        --token-code "$1")
-    export AWS_ACCESS_KEY_ID=$(echo "$CREDS" | jq -r .Credentials.AccessKeyId)
-    export AWS_SECRET_ACCESS_KEY=$(echo "$CREDS" | jq -r .Credentials.SecretAccessKey)
-    export AWS_SESSION_TOKEN=$(echo "$CREDS" | jq -r .Credentials.SessionToken)
-}
-
-aras () {
-    CREDS=$(aws sts assume-role \
-        --role-arn arn:aws:iam::766970670128:role/admin \
-        --role-session-name AdminSean \
-        --duration-seconds 3600 \
-        --serial-number arn:aws:iam::766970670128:mfa/sean \
         --token-code "$1")
     export AWS_ACCESS_KEY_ID=$(echo "$CREDS" | jq -r .Credentials.AccessKeyId)
     export AWS_SECRET_ACCESS_KEY=$(echo "$CREDS" | jq -r .Credentials.SecretAccessKey)
