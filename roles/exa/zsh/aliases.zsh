1e96943b2afd820e02f1337e6992da31b904919e
Sean Lingren <srlingren@gmail.com>
2018-10-07 11:27:41 -0400

    use exa

---
+++ roles/exa/zsh/aliases.zsh
@@ -0,0 +1,14 @@
+if $(exa &>/dev/null)
+then
+  alias l="exa -abghl"
+  alias la="exa -abghl"
+  alias ll="exa -bghl"
+  alias ls="exa"
+  lt() {
+    if [ -z "$1" ]; then
+      exa -bghlT
+    else
+      exa -bghlT -L $1
+    fi
+  }
+fi
--- roles/system/zsh/aliases.zsh
+++
@@ -1,7 +0,0 @@
-if $(gls &>/dev/null)
-then
-  alias l="gls -lAh --color"
-  alias la="gls -A --color"
-  alias ll="gls -l --color"
-  alias ls="gls -F --color"
-fi
