16b4220723728587dc1890e64465283a83ce0d64
Sean Lingren <srlingren@gmail.com>
2018-10-26 15:32:00 -0400

    mac aliases

--- roles/mac/zsh/aliases.zsh
+++ roles/mac/zsh/aliases.zsh
@@ -1,1 +1,13 @@
+[ -d ~/Downloads ]            && alias dl='cd ~/Downloads'
+[ -d ~/Projects ]             && alias pj='cd ~/projects'
+
 alias cb='pbcopy'
+
+alias o='open'
+alias oo='open .'
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
