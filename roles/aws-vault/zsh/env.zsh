afa91d2983aa656254c4fc763251de3fab3a21b9
Sean Lingren <srlingren@gmail.com>
2019-05-12 19:48:52 -0400

    aws

---
+++ roles/aws-vault/zsh/env.zsh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export AWS_VAULT_BACKEND=keychain
+export AWS_VAULT_KEYCHAIN_NAME=login
--- roles/aws-vault/zsh/functions.zsh
+++ roles/aws-vault/zsh/functions.zsh
@@ -2,7 +2,7 @@
 
 # avx -> aws-vault export
 # Takes in a profile name and exports the generated keys
-avx () {
+avx() {
     unset AWS_VAULT
     aws-vault exec --session-ttl=4h --assume-role-ttl=1h "$1"
 }
