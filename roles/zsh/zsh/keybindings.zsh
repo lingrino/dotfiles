e8d140bcb433e5a47289a74d2988de025dae4f5f
Sean Lingren <sean@lingrino.com>
2019-09-27 08:28:57 -0700

    switch to starship

--- roles/zsh/zsh/keybindings.zsh
+++ roles/zsh/zsh/keybindings.zsh
@@ -5,9 +5,9 @@ autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
-bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[A" up-line-or-beginning-search   # Up
 bindkey "^[[B" down-line-or-beginning-search # Down
 
 # Make cmd+left/right go to beginning and end of lines
 bindkey "^[OH" beginning-of-line # cmd+left
-bindkey "^[OF" end-of-line # cmd+left
+bindkey "^[OF" end-of-line       # cmd+right
