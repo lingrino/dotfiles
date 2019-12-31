ad49f3a74a7193895dc641ec9959791980162a28
Sean Lingren <sean@lingrino.com>
2019-12-31 11:10:20 -0600

    tmux changes

--- roles/gotop/zsh/aliases.zsh
+++ roles/gotop/zsh/aliases.zsh
@@ -1,3 +1,3 @@
 #!/usr/local/bin/zsh
 
-alias top='htop'
+alias top='gotop'
--- roles/tmux/zsh/aliases.zsh
+++ roles/tmux/zsh/aliases.zsh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/zsh
 
 alias t='tmux'
-alias ta='tmux new-session -A -s main && ls'
+alias ta='tmux new-session -A -s main'
