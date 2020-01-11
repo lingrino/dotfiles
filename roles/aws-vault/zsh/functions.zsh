b1eee0e428ebd63a9c63d85dee9f53932835f1ff
Sean Lingren <sean@lingrino.com>
2020-01-11 17:19:00 -0600

    use-duration

--- roles/aws-vault/zsh/functions.zsh
+++ roles/aws-vault/zsh/functions.zsh
@@ -4,5 +4,5 @@
 # Takes in a profile name and exports the generated keys
 avx() {
     unset AWS_VAULT
-    aws-vault exec --session-ttl=4h --assume-role-ttl=1h "$1"
+    aws-vault exec --duration=1h "$1"
 }
