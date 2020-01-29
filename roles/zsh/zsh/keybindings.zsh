8987809fad804222be2ab6698557d027a0772806
Sean Lingren <sean@lingrino.com>
2020-01-28 20:09:40 -0600

    add bindkey

--- roles/zsh/zsh/keybindings.zsh
+++ roles/zsh/zsh/keybindings.zsh
@@ -11,6 +11,8 @@ autoload -U up-line-or-beginning-search
 autoload -U down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search   # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
 
 # Make cmd+right/left go to beginning and end of lines.
 # You must also set your terminal emulator to send these
