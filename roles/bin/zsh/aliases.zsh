87535219d713d762522af134ff3c7517fa81c69c
Sean Lingren <sean@lingrino.com>
2020-06-06 14:53:47 -0500

    no tmux

--- roles/bin/zsh/aliases.zsh
+++ roles/bin/zsh/aliases.zsh
@@ -1,3 +1,3 @@
 #!/usr/local/bin/zsh
 
-alias reload='source ~/.zshrc && tmux source-file ~/.tmux.conf && clear'
+alias reload='source ~/.zshrc && clear'
--- roles/tmux/zsh/aliases.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/zsh
-
-alias t='tmux'
-alias ta='tmux -CC new -A -s main'
