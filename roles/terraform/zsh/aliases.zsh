4e3efcdc527133563fd2fbcc7854798e32e2b6cc
Sean Lingren <sean@lingrino.com>
2020-10-15 10:44:55 -0700

    tf aliases

--- roles/terraform/zsh/aliases.zsh
+++ roles/terraform/zsh/aliases.zsh
@@ -4,28 +4,22 @@
 alias tf='terraform'
 
 # Shorten terraform fmt, include tfvars files
-alias tff='find * -type f -name "terraform.tfvars" -exec terraform fmt {} \; && terraform fmt'
+alias tff='terraform fmt'
 
 # Shorten terraform init
 alias tfi='terraform init -upgrade'
 
-# Shorten terraform get
-alias tfg='terraform get -update'
-
 # Shorten terraform plan
-alias tfp='terraform plan -refresh=true'
+alias tfp='terraform plan'
 
 # Shorten terraform apply
-alias tfa='terraform apply -refresh=true'
+alias tfa='terraform apply'
 
 # Shorten terraform console
 alias tfc='terraform console'
 
 # Shorten terraform state
 alias tfs='terraform state'
-
-# Shorten terraform refresh
-alias tfr='terraform refresh'
 
 # Shorten terraform destroy
 alias tfd='terraform destroy'
