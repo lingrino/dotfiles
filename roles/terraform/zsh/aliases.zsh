5285ba186627a8b5dd10042b639fa5d496692982
Sean Lingren <sean@lingrino.com>
2021-04-21 15:00:38 -0700

    gh and tg aliases

--- roles/terraform/zsh/aliases.zsh
+++ roles/terraform/zsh/aliases.zsh
@@ -2,30 +2,40 @@
 
 # Shorten terraform
 alias tf='terraform'
+alias tg='terrafgrunt'
 
 # Shorten terraform fmt, include tfvars files
 alias tff='terraform fmt'
+alias tgf='terragrunt fmt'
 
 # Shorten terraform init
 alias tfi='terraform init -upgrade'
+alias tgi='terragrunt init -upgrade'
 
 # Shorten terraform import
 alias tfim='terraform import'
+alias tgim='terrgruntm import'
 
 # Shorten terraform plan
 alias tfp='terraform plan'
+alias tgp='terragrunt plan'
 
 # Shorten terraform apply
 alias tfa='terraform apply'
+alias tga='terragrunt apply'
 
 # Shorten terraform console
 alias tfc='terraform console'
+alias tgc='terragrunt console'
 
 # Shorten terraform state
 alias tfs='terraform state'
+alias tgs='terragrunt state'
 
 # Shorten terraform destroy
 alias tfd='terraform destroy'
+alias tgd='terragrunt destroy'
 
 # Shorten terraform init & apply
 alias tfia='tfi && tfa'
+alias tgia='tgi && tga'
