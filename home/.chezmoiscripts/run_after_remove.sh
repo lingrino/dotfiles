cc15fdd2782f2c6607f45541cb20937288f175ce
Sean Lingren <sean@lingren.com>
2024-11-11 19:45:18 -0800

    fixes

--- home/.chezmoiscripts/run_after_remove.sh
+++ home/.chezmoiscripts/run_after_remove.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-sudo rm -f /Applications/GarageBand.app
-sudo rm -f /Applications/iMovie.app
-sudo rm -f /Applications/Keynote.app
-sudo rm -f /Applications/Numbers.app
-sudo rm -f /Applications/Pages.app
+sudo rm -rf /Applications/GarageBand.app
+sudo rm -rf /Applications/iMovie.app
+sudo rm -rf /Applications/Keynote.app
+sudo rm -rf /Applications/Numbers.app
+sudo rm -rf /Applications/Pages.app
+sudo rm -rf /Applications/Tips.app
