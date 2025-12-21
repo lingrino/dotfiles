0942ba03f167750bcdd7ce9ae0be518815413178
Sean Lingren <sean@lingren.com>
2025-12-21 00:02:51 -0800

    fnmquiet

--- home/exact_private_dot_config/private_fish/private_conf.d/private_fnm.fish
+++ home/exact_private_dot_config/private_fish/private_conf.d/private_fnm.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-fnm env --use-on-cd --corepack-enabled --resolve-engines --version-file-strategy=recursive --shell=fish | source
+fnm env --use-on-cd --corepack-enabled --resolve-engines --version-file-strategy=recursive --log-level=quiet --shell=fish | source
