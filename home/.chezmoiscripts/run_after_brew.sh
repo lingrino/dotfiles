54bd4ed6c242b855d7f096d87bd9b4a2cfdcdc55
Sean Lingren <sean@lingren.com>
2026-08-26 11:46:57 -0700

    work6

--- home/.chezmoiscripts/run_after_brew.sh
+++ home/.chezmoiscripts/run_after_brew.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-brew update
-brew bundle --global | grep -v 'Using' || true
+brew update 2>&1 | sed -e '/^==> Updating Homebrew\.\.\.$/d' -e '/^Already up-to-date\.$/d'
+brew bundle --global | sed -e '/^Using /d' -e '/^`brew bundle` complete!/d'
 brew bundle cleanup --global --force --zap
 brew upgrade --yes
 brew cleanup
